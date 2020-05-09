634dc25f247f43d3652874f3c14d1856e6334a11
Venkat S. Narayana <narayana.svenkat.cse17@iitbhu.ac.in>
2020-05-09 21:11:59 +0530

    tb 7

--- models/bilinear.py
+++ models/bilinear.py
@@ -57,7 +57,7 @@ class BILINEAR(nn.Module):
             for member in members_embeds:
                 xmember = member.view(1,member.shape[0])
                 at_wt.append(self.attention(xmember, item_embeds))
-            at_wt = [float(i)/sum(at_wt) for i in at_wt]
+            at_wt = [float(k)/sum(at_wt) for k in at_wt]
             final_user = torch.zeros([32])
             i=0
             for member in members_embeds:
