d6e5f7f0d43368f3b9e1e14d41a10299164d3e27
Venkat S. Narayana <narayana.svenkat.cse17@iitbhu.ac.in>
2020-05-14 21:54:14 +0530

    remove bahdnau weight normalization

--- models/bahdanau.py
+++ models/bahdanau.py
@@ -20,13 +20,13 @@ class BAHDANAU(nn.Module):
         self.num_groups = len(self.group_member_dict)
 
         # initial model
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                #nn.init.normal_(m.weight)
-                m.weight.data.fill_(0.01)
-            if isinstance(m, nn.Embedding):
-                #nn.init.xavier_normal_(m.weight)
-                m.weight.data.fill_(0.01)
+        # for m in self.modules():
+        #     if isinstance(m, nn.Linear):
+        #         #nn.init.normal_(m.weight)
+        #         m.weight.data.fill_(0.01)
+        #     if isinstance(m, nn.Embedding):
+        #         #nn.init.xavier_normal_(m.weight)
+        #         m.weight.data.fill_(0.01)
 
     def forward(self, group_inputs, user_inputs, item_inputs):
 
