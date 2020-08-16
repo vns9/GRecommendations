726713cb9eea709b17f20da3dce98e6f80810630
Venkat S. Narayana <narayana.svenkat.cse17@iitbhu.ac.in>
2020-08-16 14:50:56 +0530

    bahdanau2

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ if __name__ == '__main__':
     test_loss_list = []
     '''
     # BILINEAR MODEL-----------------------------------------------------------------------------------------------------------------
-    '''
+    
     bilinear = BILINEAR(num_users, num_items, num_group, configuration.embedding_size, g_m_d, configuration.drop_ratio, genres)
     t=time()
     
@@ -153,7 +153,7 @@ if __name__ == '__main__':
 
     train_loss_list = []
     test_loss_list = []
-    '''
+    
     # BENCHMARK MODEL-----------------------------------------------------------------------------------------------------------------
     
     bahdanau = bahdanau2(num_users, num_items, num_group, configuration.embedding_size, g_m_d, configuration.drop_ratio, genres)
